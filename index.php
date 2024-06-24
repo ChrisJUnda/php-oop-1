6d6d5d96b04612f94005dd00347a1275321825ce
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-06-24 17:55:15 +0200

    [UPD] Base dati creata

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-echo 'Ciao gente';
+class Movie
+{
+    public $titolo;
+    public $genere;
+    public $anno;
+    public $lingua;
+    public $valutazione;
+
+    public function __construct($dato1, $dato2, $dato3, $dato4, $dato5)
+    {
+        $this->titolo = $dato1;
+        $this->genere = $dato2;
+        $this->anno = $dato3;
+        $this->lingua = $dato4;
+        $this->valutazione = $dato5;
+    }
+}
 ?>
 
 <!DOCTYPE html>
